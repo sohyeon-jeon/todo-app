f59748427098bd864ed48a36664dd2dbaf5ba6bf
sohyeon-jeon <wjsthgus511@naver.com>
2022-03-06 23:10:29 +0900

    optimize using useReducer

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState, useRef, useCallback, useReducer } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
-function createBulkTodos(){
-  const array=[];
-  for(let i=1;i<=2500;i++){
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
     array.push({
-      id:i,
-      text:`할 일 ${i}`,
-      checked:false,
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
     });
   }
   return array;
 }
 
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT':
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      return todos.filter(todo => todo.id !== action.id);
+    case 'TOGGLE':
+      return todos.map(todo =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo);
+    default:
+      return todos;
+  }
+}
+
 const App = () => {
-  const [todos,setTodos]=useState(createBulkTodos);
-  // const [todos, setTodos] = useState([
-  //   {
-  //     id: 1,
-  //     text: '리액트의 기초 알아보기',
-  //     checked: true,
-  //   },
-  //   {
-  //     id: 2,
-  //     text: '컴포넌트 스타일링해 보기',
-  //     checked: true,
-  //   },
-  //   {
-  //     id: 3,
-  //     text: '일정 관리 앱 만들어 보기',
-  //     checked: false,
-  //   },
-  // ]);
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+  // const [todos,setTodos]=useState(createBulkTodos);
 
   // 고유 값으로 사용 될 id
   // ref 를 사용하여 변수 담기
@@ -46,7 +44,8 @@ const App = () => {
         text,
         checked: false,
       };
-      setTodos(todos=>todos.concat(todo));
+      dispatch({ type: 'INSERT', todo });
+      // setTodos(todos=>todos.concat(todo));
       nextId.current += 1; // nextId 1 씩 더하기
     },
     [],
@@ -54,18 +53,20 @@ const App = () => {
 
   const onRemove = useCallback(
     id => {
-      setTodos(todos=>todos.filter(todo => todo.id !== id));
+      dispatch({ type: 'REMOVE', id })
+      // setTodos(todos=>todos.filter(todo => todo.id !== id));
     },
     [],
   );
 
   const onToggle = useCallback(
     id => {
-      setTodos(todos=>
-        todos.map(todo =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
+      dispatch({ type: 'TOGGLE', id })
+      // setTodos(todos=>
+      //   todos.map(todo =>
+      //     todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+      //   ),
+      // );
     },
     [],
   );
