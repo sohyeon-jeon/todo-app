0191eb92853b35adc3deef19144cb2b3ae20dc9b
sohyeon-jeon <wjsthgus511@naver.com>
2022-03-07 00:05:54 +0900

    optimize using react-virtualized

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React,{useCallback} from "react";
 import TodoListItem from "./TodoListItem";
 import './TodoList.scss';
+import { List } from "../../node_modules/react-virtualized/dist/commonjs/index";
 
 const TodoList = ({ todos,onRemove,onToggle }) => {
+    const rowRenderer=useCallback(
+        ({index,key,style})=>{
+            const todo=todos[index];
+            return(
+                <TodoListItem
+                todo={todo}
+                key={key}
+                onRemove={onRemove}
+                onToggle={onToggle}
+                style={style}
+                />
+            )
+        },
+        [onRemove,onToggle,todos],
+    )
     return (
-        <div className="TodoList">
-            {todos.map(todo => (
-        <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle}/>
-            ))}
-        </div>
+        <List
+        className="TodoList"
+        width={512}
+        height={513}
+        rowCount={todos.length}
+        rowHeight={57}
+        rowRenderer={rowRenderer}
+        list={todos}
+        style={{outline:'none'}}
+        />
+        // <div className="TodoList">
+        //     {todos.map(todo => (
+        // <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle}/>
+        //     ))}
+        // </div>
     )
 }
 
-export default TodoList;
-
+export default React.memo(TodoList);
